2c60158460b546b8c7c09b0afb77f44593567b83
Sanhu Li <lisanhu2014@hotmail.com>
2024-03-12 04:29:10 -0400

    Initial commit

---
+++ Source/Main.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+// using System.Reflection;
+using HarmonyLib;
+
+namespace LaoBa
+{
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            Log.Message("LaoBa's RimWorld loaded successfully!");
+        }
+    }
+
+}
